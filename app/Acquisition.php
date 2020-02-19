44baaab1b7e3c64ede30092f91dd8ac18d910b5d
David Gray <grayda@solidinc.org>
2020-02-19 23:57:01 +1100

    Add an acquisition migration

---
+++ app/Acquisition.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Acquisition extends Model
+{
+    //
+}
---
+++ database/migrations/2020_02_19_125602_create_acquisitions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAcquisitionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('acquisitions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title'); // The title of this acquisition type (e.g. "Facebook Marketplace", "eBay" etc.)
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('acquisitions');
+    }
+}
