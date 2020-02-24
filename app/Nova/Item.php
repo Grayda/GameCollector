49be7c9005f76d7eb1468f5c2e2fd7d5adea3d71
David Gray <grayda@solidinc.org>
2020-02-24 21:20:32 +1100

    Revert "Australian formatting for the date."
    This reverts commit 728246efb3423611bea5253a8a750979f64d2a21.

--- app/Nova/Item.php
+++ app/Nova/Item.php
@@ -123,8 +123,6 @@ class Item extends Resource
             Heading::make('Acquisition Details'),
             Date::make('Date Acquired', 'acquired_at')
               ->nullable()
-              ->format('MMM Do Y')
-              ->pickerFormat('d-m-Y')
               ->help('When was this item acquired?'),
             BelongsTo::make('Acquisition Method', 'acquisition', 'App\Nova\Acquisition')
               ->withoutTrashed()
