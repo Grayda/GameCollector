6d50de843bd96113e49ae3a3f1b64a53a61f0ab8
David Gray <grayda@solidinc.org>
2020-02-25 15:48:09 +1100

    Add some cards to the dashboard

---
+++ app/Nova/Metrics/CollectionConditionBreakdown.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Nova\Metrics;
+
+use App\Item;
+use App\Condition;
+
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Metrics\Partition;
+
+class CollectionConditionBreakdown extends Partition
+{
+    /**
+     * Calculate the value of the metric.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return mixed
+     */
+    public function calculate(NovaRequest $request)
+    {
+      return $this->count($request, Item::class, 'condition_id')
+        ->label(function($value) {
+          return Condition::where('id', $value)->first()->title;
+        });
+    }
+
+    /**
+     * Determine for how many minutes the metric should be cached.
+     *
+     * @return  \DateTimeInterface|\DateInterval|float|int
+     */
+    public function cacheFor()
+    {
+        // return now()->addMinutes(5);
+    }
+
+    /**
+     * Get the URI key for the metric.
+     *
+     * @return string
+     */
+    public function uriKey()
+    {
+        return 'collection-condition-breakdown';
+    }
+}
