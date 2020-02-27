7429ac77c1cc2a5204e1ac968dbcad134b04cc15
David Gray <grayda@solidinc.org>
2020-02-27 23:02:20 +1100

    Tweak permissions, mostly for the tag resource

--- app/Policies/BasePolicy.php
+++ app/Policies/BasePolicy.php
@@ -10,98 +10,18 @@ class BasePolicy
     use HandlesAuthorization;
 
     /**
-     * Determine whether the user can view any base1s.
+     * Create a new policy instance.
      *
-     * @param  \App\User  $user
-     * @return mixed
+     * @return void
      */
-    public function viewAny(User $user)
-    {
-        if ($user->is_admin === true) {
-            return true;
-        }
-    }
+     public function viewAny(User $user)
+     {
+         return true;
+     }
 
-    /**
-     * Determine whether the user can view the base1.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Model  $resource
-     * @return mixed
-     */
-    public function view(User $user, $resource)
-    {
-        if ($user->is_admin === true) {
-            return true;
-        }
-    }
+     public function view(User $user, $resource)
+     {
+         return !is_null($user->email_verified_at);
+     }
 
-    /**
-     * Determine whether the user can create base1s.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
-    public function create(User $user)
-    {
-        if ($user->is_admin === true) {
-            return true;
-        }
-    }
-
-    /**
-     * Determine whether the user can update the base1.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Model  $resource
-     * @return mixed
-     */
-    public function update(User $user, $resource)
-    {
-        if ($user->is_admin === true) {
-            return true;
-        }
-    }
-
-    /**
-     * Determine whether the user can delete the base1.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Model  $resource
-     * @return mixed
-     */
-    public function delete(User $user, $resource)
-    {
-        if ($user->is_admin === true) {
-            return true;
-        }
-    }
-
-    /**
-     * Determine whether the user can restore the base1.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Model  $resource
-     * @return mixed
-     */
-    public function restore(User $user, $resource)
-    {
-        if ($user->is_admin === true) {
-            return true;
-        }
-    }
-
-    /**
-     * Determine whether the user can permanently delete the base1.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Model  $resource
-     * @return mixed
-     */
-    public function forceDelete(User $user, $resource)
-    {
-        if ($user->is_admin === true) {
-            return true;
-        }
-    }
 }
