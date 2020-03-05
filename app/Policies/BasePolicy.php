37ca6137c614a2f27f3805192a36a018ff8c6bf6
David Gray <grayda@solidinc.org>
2020-03-05 22:57:38 +1100

    Fix the policy again.

--- app/Policies/BasePolicy.php
+++ app/Policies/BasePolicy.php
@@ -10,18 +10,98 @@ class BasePolicy
     use HandlesAuthorization;
 
     /**
-     * Create a new policy instance.
+     * Determine whether the user can view any resource.
      *
-     * @return void
+     * @param  \App\User  $user
+     * @return mixed
      */
-     public function viewAny(User $user)
-     {
-         return true;
-     }
+    public function viewAny(User $user)
+    {
+        if ($user->is_admin === true) {
+            return true;
+        }
+    }
 
-     public function view(User $user, $resource)
-     {
-         return !is_null($user->email_verified_at);
-     }
+    /**
+     * Determine whether the user can view the resource.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Model  $resource
+     * @return mixed
+     */
+    public function view(User $user, $resource)
+    {
+        if ($user->is_admin === true) {
+            return true;
+        }
+    }
 
+    /**
+     * Determine whether the user can create resource.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        if ($user->is_admin === true) {
+            return true;
+        }
+    }
+
+    /**
+     * Determine whether the user can update the resource.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Model  $resource
+     * @return mixed
+     */
+    public function update(User $user, $resource)
+    {
+        if ($user->is_admin === true) {
+            return true;
+        }
+    }
+
+    /**
+     * Determine whether the user can delete the resource.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Model  $resource
+     * @return mixed
+     */
+    public function delete(User $user, $resource)
+    {
+        if ($user->is_admin === true) {
+            return true;
+        }
+    }
+
+    /**
+     * Determine whether the user can restore the resource.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Model  $resource
+     * @return mixed
+     */
+    public function restore(User $user, $resource)
+    {
+        if ($user->is_admin === true) {
+            return true;
+        }
+    }
+
+    /**
+     * Determine whether the user can permanently delete the resource.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Model  $resource
+     * @return mixed
+     */
+    public function forceDelete(User $user, $resource)
+    {
+        if ($user->is_admin === true) {
+            return true;
+        }
+    }
 }
