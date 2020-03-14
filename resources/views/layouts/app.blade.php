d1693bc59c999fb4dbe61e02914fbd486405aded
David Gray <grayda@solidinc.org>
2020-03-15 02:47:25 +1100

    Add a /home link to the nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,6 +62,9 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('home') }}">
+                                        {{ __('Your Profile') }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
