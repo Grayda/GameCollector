e5be83704f1593e533cc64cd970dde603c5fd3c8
David Gray <grayda@solidinc.org>
2020-03-14 17:39:42 +1100

    Add cancellation box on form

--- resources/views/subscription/cancel.blade.php
+++ resources/views/subscription/cancel.blade.php
@@ -10,8 +10,25 @@
       <ul>
         <li>Your subscription will continue until {{ Carbon\Carbon::createFromTimestamp(auth()->user()->subscription()->asStripeSubscription()->current_period_end)->format('D jS F h:ia T') }}</li>
         <li>When your subscription ends, we'll keep your items for 6 months. Be sure to download your items if you want to keep them!</li>
+        <li>You can resume your subscription by returning to this page before {{ Carbon\Carbon::createFromTimestamp(auth()->user()->subscription()->asStripeSubscription()->current_period_end)->format('D jS F h:ia T') }}</li>
+        <li>If you have time, please send an email to <a href="mailto:{{ config('mail.from.address') }}">{{ config('mail.from.address') }}</a> and let us know why you're cancelling. This will help us improve in the future</li>
       </ul>
+      If you're sure you want to cancel your subscription, type <b>cancel</b> (lowercase) into the box and click the button.
     </p>
-
+  </div>
+  <div class="container text-center">
+    @error('cancel')
+      <div class="alert alert-danger">You need to type <code>cancel</code> into the box to confirm that you wish to cancel!</div>
+    @enderror
+    @error('issue')
+      <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
+    <form action="/subscription/cancel" method="post" class="form-group">
+      @csrf
+      <div class="col-6 offset-3">
+        <input type="text" class="form-control" placeholder="cancel" id="cancel" name="cancel">
+      </div>
+      <button type="submit" class="btn btn-lg btn-danger">Cancel my subscription</button>
+    </form>
   </div>
 @endsection
