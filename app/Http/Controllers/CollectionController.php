8b304eba467ffec58fa1425f009ccf2fad65c2b3
David Gray <grayda@solidinc.org>
2020-03-17 15:13:28 +1100

    Allow the user to override the layout via a query string.
    Mostly used for testing, but might be useful for prepping screenshots or whatever.

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -8,7 +8,15 @@ use App\Collection;
 class CollectionController extends Controller
 {
     function view(Request $request, String $uuid) {
+      $request->validate([
+        'layout' => 'in:cards,list'
+      ]);
+
       $collection = Collection::where('collection_id', $uuid)->where('public', true)->with(['items', 'owner'])->firstOrFail();
+
+      if(!empty($request->query('layout'))) {
+        $collection->layout = $request->query('layout');
+      }
 
       return view('collections.' . ($collection->layout ?? 'cards'), ['collection' => $collection]);
     }
