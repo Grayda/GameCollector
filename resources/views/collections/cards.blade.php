78f41e78626fe6a3d9733dc52375377890143107
David Gray <grayda@solidinc.org>
2020-03-18 17:43:29 +1100

    Make cards work better on slightly smallish screens

--- resources/views/collections/cards.blade.php
+++ resources/views/collections/cards.blade.php
@@ -9,7 +9,7 @@
     <div class="row">
     @foreach($collection['items'] as $game)
 
-      <div class="col-md-3">
+      <div class="col-lg-3">
       <div class="card">
         @if($collection->fields['images'] ?? false)
           <img src="{{ $game->getFirstMediaUrl('', 'medium-size') }}" class="card-img-top img-fluid">
