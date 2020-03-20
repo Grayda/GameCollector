33050855cd4a71a4ca5b6632217c3b7f9ad6c857
David Gray <grayda@solidinc.org>
2020-03-20 15:57:18 +1100

    Add active flag for plans partial
    That way you can just show the plans without having them clickable.

--- resources/views/partials/plans.blade.php
+++ resources/views/partials/plans.blade.php
@@ -2,11 +2,13 @@
   @foreach(collect(config('access.tiers'))->where('selectable') as $key => $tier)
     <div class="col">
         <div class="subscription-option">
-            <input class="{{ $key }}" type="radio" id="{{ $key }}" name="plan" value="{{ $key }}" {{ auth()->user()->plan == $key ? 'checked' : '' }} {{ auth()->user()->plan == "none" && $tier['default'] ? 'checked' : '' }}>
+            @if($active ?? true)
+              <input class="{{ $key }}" type="radio" id="{{ $key }}" name="plan" value="{{ $key }}" {{ auth()->user()->plan == $key ? 'checked' : '' }} {{ auth()->user()->plan == "none" && $tier['default'] ? 'checked' : '' }}>
+            @endif
             <label for="{{ $key }}">
                 <span class="plan-icon"><i class="{{ $tier['icon'] }}"></i></span>
                 <span class="plan-price">${{ $tier['price'] }} <small>/month</small></span>
-                <span class="plan-trial">{{ $tier['trial'] }} day trial</span>
+                <span class="plan-trial">+{{ $tier['trial'] }} day trial</span>
                 <span class="plan-name">{{ $tier['name'] }}</span>
                 <span class="plan-description">
                   <b>Items:</b> {{ $tier['limit'] < 0 ? 'Unlimited' : $tier['limit'] }}<br />
--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -3,25 +3,7 @@
 @section('content')
 <div class="container text-center">
     <h1>Game Collector Pricing</h1>
-    <p>Game Collector has {{ collect(config('access.tiers'))->where('selectable')->count() }} plans available, with different limits</p>
-    <p>
-    <div class="row">
-        @foreach(collect(config('access.tiers'))->where('selectable') as $key => $tier)
-            <div class="col">
-                <div class="subscription-option">
-                    <label for="{{ $key }}">
-                    <span class="plan-icon"><i class="{{ $tier['icon'] }}"></i></span>
-                    <span class="plan-price">${{ $tier['price'] }} <small>/month</small></span>
-                    <span class="plan-name">{{ $tier['name'] }}</span>
-                    <span class="plan-description">
-                      <b>Items:</b> {{ $tier['limit'] < 0 ? 'Unlimited' : $tier['limit'] }}<br />
-                      <b>Collections:</b> {{ $tier['collection_limit'] < 0 ? 'Unlimited' : $tier['collection_limit'] }}<br />
-                      <b>Photos:</b> {{ $tier['photos'] ? 'Yes' : 'No' }}
-                    </span>
-                </label>
-                </div>
-            </div>
-            @endforeach
-    </div>
+    <p class="lead">Game Collector has {{ collect(config('access.tiers'))->where('selectable')->count() }} plans available, with different limits</p>
+    <p>@include('partials.plans', ['active' => false])</p>
 </div>
 @endsection
