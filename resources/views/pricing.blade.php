92ea3229fca64d9fbffccad55c5b7979f10e1b90
David Gray <grayda@solidinc.org>
2020-03-23 16:21:25 +1100

    Add a signup button to the pricing page.

--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -5,5 +5,6 @@
     <h1>Game Collector Pricing</h1>
     <p class="lead">Game Collector has {{ collect(config('access.tiers'))->where('selectable')->count() }} plans available, with different limits</p>
     <p>@include('partials.plans', ['active' => false])</p>
+    <p><a class="btn btn-lg btn-success" href="/register"><i class="fas fa-gamepad"></i> Manage your collection now!</a></p>
 </div>
 @endsection
