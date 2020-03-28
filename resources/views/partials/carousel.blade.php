6e5e079273574ba2811f1114c6ccca7a0dbff6a5
David Gray <grayda@solidinc.org>
2020-03-28 22:40:53 +1100

    Don't show indicators / controls if we only have 1 media item.

--- resources/views/partials/carousel.blade.php
+++ resources/views/partials/carousel.blade.php
@@ -1,9 +1,11 @@
 <div id="carousel-{{ $images->first()->id }}" class="carousel slide" data-interval="false">
-  <ol class="carousel-indicators">
-    @foreach($images as $key => $image)
-      <li data-target="#carousel-{{ $images->first()->id }}" data-slide-to="{{ $key }}" class="@if($loop->first) active @endif"></li>
-    @endforeach
-  </ol>
+  @if($images->count() > 1)
+    <ol class="carousel-indicators">
+      @foreach($images as $key => $image)
+        <li data-target="#carousel-{{ $images->first()->id }}" data-slide-to="{{ $key }}" class="@if($loop->first) active @endif"></li>
+      @endforeach
+    </ol>
+  @endif
   <div class="carousel-inner">
     @foreach($images as $image)
       <div class="carousel-item @if($loop->first) active @endif">
@@ -16,12 +18,14 @@
       </div>
     @endforeach
   </div>
-  <a class="carousel-control-prev" href="#carousel-{{ $images->first()->id }}" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carousel-{{ $images->first()->id }}" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+  @if($images->count() > 1)
+    <a class="carousel-control-prev" href="#carousel-{{ $images->first()->id }}" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carousel-{{ $images->first()->id }}" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  @endif
 </div>
