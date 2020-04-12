45cbb806a4d3bbadb9540e00d5d64bc6456f8a25
David Gray <grayda@solidinc.org>
2020-04-12 14:34:28 +1000

    Only show carousel if there's actually items to show.

--- resources/views/partials/carousel.blade.php
+++ resources/views/partials/carousel.blade.php
@@ -1,31 +1,33 @@
-<div id="carousel-{{ $images->first()->id }}" class="carousel slide" data-interval="false">
-  @if($images->count() > 1)
-    <ol class="carousel-indicators">
-      @foreach($images as $key => $image)
-        <li data-target="#carousel-{{ $images->first()->id }}" data-slide-to="{{ $key }}" class="@if($loop->first) active @endif"></li>
+@if(!$images->isEmpty())
+  <div id="carousel-{{ $images->first()->id }}" class="carousel slide" data-interval="false">
+    @if($images->count() > 1)
+      <ol class="carousel-indicators">
+        @foreach($images as $key => $image)
+          <li data-target="#carousel-{{ $images->first()->id }}" data-slide-to="{{ $key }}" class="@if($loop->first) active @endif"></li>
+        @endforeach
+      </ol>
+    @endif
+    <div class="carousel-inner">
+      @foreach($images as $image)
+        <div class="carousel-item @if($loop->first) active @endif">
+          <img src="{{ $image->getUrl('medium-size') }}" class="d-block w-100 img-fluid" alt="">
+          @if($image->getCustomProperty('description') ?? null)
+            <div class="carousel-caption d-none d-md-block">
+              <p> {{ $image->getCustomProperty('description') ?? null }}</p>
+            </div>
+          @endif
+        </div>
       @endforeach
-    </ol>
-  @endif
-  <div class="carousel-inner">
-    @foreach($images as $image)
-      <div class="carousel-item @if($loop->first) active @endif">
-        <img src="{{ $image->getUrl('medium-size') }}" class="d-block w-100 img-fluid" alt="">
-        @if($image->getCustomProperty('description') ?? null)
-          <div class="carousel-caption d-none d-md-block">
-            <p> {{ $image->getCustomProperty('description') ?? null }}</p>
-          </div>
-        @endif
-      </div>
-    @endforeach
+    </div>
+    @if($images->count() > 1)
+      <a class="carousel-control-prev" href="#carousel-{{ $images->first()->id }}" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carousel-{{ $images->first()->id }}" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    @endif
   </div>
-  @if($images->count() > 1)
-    <a class="carousel-control-prev" href="#carousel-{{ $images->first()->id }}" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carousel-{{ $images->first()->id }}" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  @endif
-</div>
+@endif
