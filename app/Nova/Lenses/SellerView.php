79e4cd36ac4a267f42d3a4d85cb7aa253f0a9f51
David Gray <david@davidgray.photography>
2020-08-18 23:46:08 +1000

    Order lens by updated_at

--- app/Nova/Lenses/SellerView.php
+++ app/Nova/Lenses/SellerView.php
@@ -30,6 +30,7 @@ class SellerView extends Lens
               ->withTrashed()
               ->whereJsonContains('tags', 'Sold')
               ->orWhereJsonContains('tags', 'Selling')
+              ->orderBy('updated_at')
               ->orderByRaw('CASE WHEN tags LIKE \'%Selling%\' THEN 1 ELSE 2 END') // Sort by Selling tag first
               ->mine()
         ));
